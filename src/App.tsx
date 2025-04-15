b8be564f62ce706fceaa2f622d0cdff8567b8cc7
Phan Trung Kien <phantrungkien152004@gmail.com>
2025-04-15 23:36:27 +0700

    CRUD todoApp

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,35 @@ import {
   Button,
   Checkbox,
   colors,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   FormControl,
   Grid,
   InputLabel,
-  MenuItem,
   Select,
+  TextField,
   Typography,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import LaptopMacIcon from "@mui/icons-material/LaptopMac";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { SetStateAction, useState } from "react";
+import "./App.css";
 function App() {
-  var todoList = [
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [todoList, setTodoList] = useState([
     {
       title: "Create a react project",
       time: "5:23 AM, 01/06/2022",
@@ -29,17 +45,66 @@ function App() {
       title: "Learn React",
       time: "5:22 AM, 01/06/2022",
       isCheck: false,
-      icon: <FavoriteBorderIcon></FavoriteBorderIcon>,
+      icon: <FavoriteBorderIcon />,
     },
     {
       title: "Create a Todo App",
       time: "5:21 AM, 01/06/2022",
       isCheck: true,
-      icon: <LaptopMacIcon></LaptopMacIcon>,
+      icon: <LaptopMacIcon />,
     },
-  ];
+  ]);
+
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  const handleAddTask = () => {
+    const date = new Date();
+    const newTask = {
+      title: newTaskTitle,
+      time:
+        date.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        }) +
+        ", " +
+        date.toLocaleDateString(),
+      isCheck: newTaskIsCheck,
+    };
+    setTodoList([...todoList, newTask]);
+    setNewTaskTitle("");
+    //setOpen(false);
+  };
+
+  const [newTodoDelete, setNewTodoDelete] = useState("");
+  const handleNewTodoDelete = (indexToDelete: number) => {
+    setTodoList(todoList.filter((todo, index) => index !== indexToDelete));
+  };
+
+  const [editIndex, setEditIndex] = useState(0); // lưu index task cần sửa
+  const [editOpen, setEditOpen] = useState(false);
+  const [newTaskIsCheck, setNewTaskIsCheck] = useState(false);
+  const handleEditClickOpen = (indexToEdit: number) => {
+    setEditIndex(indexToEdit);
+    setNewTaskTitle(todoList[indexToEdit].title);
+    setNewTaskIsCheck(todoList[indexToEdit].isCheck);
+    setEditOpen(true);
+  };
+  const handleUpdateTask = () => {
+    const updatedList = [...todoList];
+    updatedList[editIndex] = {
+      ...updatedList[editIndex],
+      title: newTaskTitle,
+      isCheck: newTaskIsCheck,
+    };
+    setTodoList(updatedList);
+    setEditOpen(false);
+  };
+  const handleCloseEdit = () => {
+    setEditOpen(false);
+  };
   return (
-    <div>
+    <div style={{ maxWidth: "75%", margin: "auto" }}>
       <Typography
         sx={{ color: "#707070", textAlign: "center" }}
         variant="h3"
@@ -55,13 +120,14 @@ function App() {
         }}
       >
         <Button
-          id="AddItem"
+          id="AddTask"
           sx={{
             backgroundColor: "#7a50f4",
             textTransform: "capitalize",
             padding: "1px 25px",
           }}
           variant="contained"
+          onClick={handleClickOpen}
         >
           Add Task
         </Button>
@@ -78,9 +144,9 @@ function App() {
             label="Content"
             sx={{ padding: "1px 60px" }}
           >
-            {/* <MenuItem value={10}>Ten</MenuItem>
-    <MenuItem value={20}>Twenty</MenuItem>
-    <MenuItem value={30}>Thirty</MenuItem> */}
+            {/* <MenuTask value={10}>Ten</MenuTask>
+    <MenuTask value={20}>Twenty</MenuTask>
+    <MenuTask value={30}>Thirty</MenuTask> */}
           </Select>
         </FormControl>
       </Box>
@@ -88,11 +154,16 @@ function App() {
       <div>
         <Box
           component="section"
-          sx={{ background: "#EEEEEE", p: 1, borderRadius: "10px" }}
+          sx={{
+            background: "#EEEEEE",
+            p: 1,
+            borderRadius: "10px",
+          }}
         >
-          {todoList.map((todo) => (
+          {todoList.map((todo, index) => (
             <>
               <div
+                key={index}
                 id="ListContent"
                 style={{
                   display: "flex",
@@ -129,21 +200,24 @@ function App() {
                   style={{ alignSelf: "center", marginLeft: "auto" }}
                   id="ListID"
                 >
-                  {" "}
                   <DeleteIcon
                     sx={{
                       backgroundColor: "#c9c9c9",
                       mr: "5px",
                       p: "2px",
                       borderRadius: "5px",
+                      cursor: "pointer",
                     }}
+                    onClick={() => handleNewTodoDelete(index)}
                   ></DeleteIcon>
                   <EditIcon
                     sx={{
                       backgroundColor: "#c9c9c9",
                       p: "2px",
                       borderRadius: "5px",
+                      cursor: "pointer",
                     }}
+                    onClick={() => handleEditClickOpen(index)}
                   ></EditIcon>
                 </div>
               </div>
@@ -151,6 +225,87 @@ function App() {
           ))}
         </Box>
       </div>
+
+      {/* Dialog Add */}
+      <Dialog
+        open={open}
+        sx={{ animation: "effect ease 0.25s" }}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{" Thêm task mới "}</DialogTitle>
+        <DialogContent>
+          <input
+            style={{
+              width: "100%",
+              padding: "10px",
+              marginTop: "10px",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+            }}
+            type="text"
+            value={newTaskTitle}
+            onChange={(e) => setNewTaskTitle(e.target.value)}
+            placeholder="Nhập tên task"
+          />
+          <Box display="flex" alignItems="center" mt={2}>
+            <Checkbox
+              checked={newTaskIsCheck}
+              onChange={(e) => setNewTaskIsCheck(e.target.checked)}
+            />
+            <Typography>Đánh dấu hoàn thành hay k</Typography>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            sx={{ backgroundColor: "#7a50f4", color: "white" }}
+            onClick={handleAddTask}
+            disabled={!newTaskTitle.trim()} // Không cho add nếu rỗng
+          >
+            Add
+          </Button>
+          <Button
+            sx={{ backgroundColor: "red", color: "white" }}
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* Dialog edit */}
+      <Dialog open={editOpen} sx={{ animation: "effect ease 0.25s" }}>
+        <DialogTitle>Chỉnh sửa Task</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Nội dung"
+            value={newTaskTitle}
+            onChange={(e) => setNewTaskTitle(e.target.value)}
+            fullWidth
+            sx={{ mt: 1 }}
+          />
+          <Box display="flex" alignItems="center" mt={2}>
+            <Checkbox
+              checked={newTaskIsCheck}
+              onChange={(e) => setNewTaskIsCheck(e.target.checked)}
+            />
+            <Typography>Đánh dấu hoàn thành hay k</Typography>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            sx={{ backgroundColor: "#2986cc", color: "white" }}
+            onClick={handleUpdateTask}
+          >
+            Save
+          </Button>
+          <Button
+            sx={{ backgroundColor: "red", color: "white" }}
+            onClick={handleCloseEdit}
+          >
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
